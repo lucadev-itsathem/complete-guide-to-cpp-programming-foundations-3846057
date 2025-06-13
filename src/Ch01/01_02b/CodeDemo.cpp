d868377a3d0a53b24097d9e0d7fc23b3d2bfb74e
lucadev-itsathem <laura.devesa1996@gmail.com>
2025-06-13 20:10:33 +0000

    código con dúas funcións

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,11 +2,19 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 
+/* Neste programa Navi
+saúdanos co seu
+característico "Hey, listen!" */
+
+// Incluímos a libraría iostream, que nos permite usar cout e endl
 #include <iostream>
 
+// Aquí comeza a función principal, onde se executa o programa
 int main(){
+    // Mostramos o texto "Hey, listen!" na pantalla seguido dun salto de liña
     std::cout << "Hey, listen!" << std::endl;
-
+    // Engadimos dúas liñas baleiras máis
     std::cout << std::endl << std::endl;
+    // Indicamos que o programa rematou correctamente
     return 0;
 }
